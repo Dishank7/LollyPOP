82d333687c3ce196239f1784ca6b3dc4d1117c71
Dishank7 <127612933+Dishank7@users.noreply.github.com>
2024-07-25 20:13:38 +0530

    sec

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ const Footer = () =>{
         <>
         <div className="footer">
           <h2>© 2023 Lollypop - A Terralogic company</h2>
+          <p>Designed By Dzank_7</p>
         </div>
         </>
     )
